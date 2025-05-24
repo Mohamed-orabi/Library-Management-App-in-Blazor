92554c9c7f7aa05ed39f770f7e8d0c552e3e1e00
Mohamed Orabi <moh.orabi97@gmail.com>
2025-05-24 20:46:56 +0300

    Updating the design

--- LibraryManagementApp.Application/DTOs/BookDto.cs
+++ LibraryManagementApp.Application/DTOs/BookDto.cs
@@ -8,6 +8,9 @@
         public DateTime PublicationDate { get; set; }
         public int CategoryId { get; set; }
         public string CategoryName { get; set; } = string.Empty;
+
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
         public List<int> AuthorIds { get; set; } = new();
     }
 }
